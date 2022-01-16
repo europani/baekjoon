d21f2c53c48eacad3ab63ded948334ab3cd22d09
SANGIL PARK <europani1212@gmail.com>
2022-01-16 18:23:28 +0900

    백준허브 업데이트

--- N과-M-(2)/N과-M-(2).py
+++
@@ -1,8 +0,0 @@
-from itertools import combinations
-
-n, m = map(int, input().split())
-nums = [i for i in range(1, n+1)]
-result = list(combinations(nums, m))
-
-for x in result:
-    print(" ".join(map(str, x)))
