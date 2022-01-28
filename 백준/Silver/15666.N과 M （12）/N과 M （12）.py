aa2af50d80dd826d658b0e65e17e524b1f9580c2
SANGIL PARK <europani1212@gmail.com>
2022-01-28 09:16:38 +0900

    [Silver II] Title: N과 M (12), Time: 80 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/15666.N과 M （12）/N과 M （12）.py
@@ -0,0 +1,8 @@
+from itertools import combinations_with_replacement
+
+n, r = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+
+perms = sorted(set(combinations_with_replacement(nums, r)))
+for perm in perms:
+    print(*perm)
