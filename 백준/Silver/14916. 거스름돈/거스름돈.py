d65e8b1a2bf5fde117f886e95c7f046b4ca7674e
SANGIL PARK <europani1212@gmail.com>
2022-03-14 17:22:54 +0900

    [Silver V] Title: 거스름돈, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -1,15 +1,12 @@
-import sys
-INF = sys.maxsize
-
 n = int(input())
-dp=[INF] * (n+1)
-dp[0]=0
+result = 0
 
-for i in range(2, n+1):
-    if i%5==0:
-        dp[i]=i//5
-    if i%2==0:
-        dp[i] = min(i//2, dp[i-2]+1, dp[i])
-    else:
-        dp[i] = min(dp[i-2]+1, dp[i])
-print(dp[n] if dp[n]!=INF else -1)
+while n>0:
+    if n%5==0:
+        result+=n//5
+        n%=5
+    else: 
+        n-=2
+        result+=1
+
+print(result if n>=0 else -1)
