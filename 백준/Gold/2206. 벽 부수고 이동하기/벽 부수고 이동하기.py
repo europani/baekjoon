8675344f26d473a440e488efe8b862c7bd577e56
SANGIL PARK <europani1212@gmail.com>
2022-03-28 21:35:56 +0900

    [Gold IV] Title: 벽 부수고 이동하기, Time: 5988 ms, Memory: 190360 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -2,43 +2,35 @@ from collections import deque
 import sys
 INF = sys.maxsize
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 n, m = map(int, input().split())
 graph = [list(map(int, input())) for _ in range(n)]
 distance = [[[INF]*2 for _ in range(m)] for _ in range(n)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y):
+def bfs(start_x, start_y):
     queue = deque()
-    queue.append((x, y))
-    distance[x][y][0] = 0
+    queue.append((start_x, start_y, 0))
+    distance[start_x][start_y][0]=1
 
     while queue:
-        x, y = queue.popleft()
+        x, y, s = queue.popleft()
+
+        if x == n-1 and y == m-1:
+            return min(distance[n-1][m-1])
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = x+dx[i]
+            ny = y+dy[i]
 
-            if nx>=n or ny>=m or nx<0 or ny<0:
-                continue
-
-            if graph[nx][ny] == 0:
-                if distance[nx][ny][0] == INF and distance[x][y][0] != INF:
-                    distance[nx][ny][0]=distance[x][y][0]+1
-                    queue.append((nx, ny))
-                if distance[nx][ny][1] == INF:
-                    distance[nx][ny][1]=distance[x][y][1]+1
-                    queue.append((nx, ny))
-            else:
-                if distance[nx][ny][1] == INF and distance[x][y][0] != INF:
-                    distance[nx][ny][1]=distance[x][y][0]+1
-                    queue.append((nx, ny))
-
-
-bfs(0, 0)
-if distance[n-1][m-1] == [INF, INF]:
-    print(-1)
-else:
-    print(min(distance[n-1][m-1])+1)
+            if 0<=nx<n and 0<=ny<m and distance[nx][ny][s] == INF:
+                if graph[nx][ny] == 0:
+                    distance[nx][ny][s] = distance[x][y][s]+1
+                    queue.append((nx, ny, s))
+                elif graph[nx][ny] == 1 and s < 1:
+                    distance[nx][ny][s+1]=distance[x][y][s]+1
+                    queue.append((nx, ny, s+1))
+    return -1
+  
+print(bfs(0, 0))
