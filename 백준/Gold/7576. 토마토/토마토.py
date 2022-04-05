7c8f9bd0d993d6693cc6bda5ce25d9b40e8a4415
SANGIL PARK <europani1212@gmail.com>
2022-04-05 13:57:02 +0900

    [Gold V] Title: 토마토, Time: 2056 ms, Memory: 96848 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,40 +1,46 @@
 import sys
 from collections import deque
-
-m, n = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+input = sys.stdin.readline
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(m)]
+
+def find_max():
+    max_day = 0
+    for i in range(m):
+        for j in range(n):
+            if graph[i][j]==0:
+                return -1
+            if graph[i][j] > max_day:
+                max_day = graph[i][j]
+
+    return max_day-1
+
 def bfs():
     queue = deque()
 
-    for i in range(n):
-        for j in range(m):
+    # 초기화
+    for i in range(m):
+        for j in range(n):
             if graph[i][j]==1:
                 queue.append((i, j))
 
     while queue:
         x, y = queue.popleft()
-        day = graph[x][y]
-        
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
 
-            if nx>=n or ny>=m or nx<0 or ny<0:
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= m or ny >= n:
                 continue
-            
-            if graph[nx][ny]==0:
-                graph[nx][ny]=day+1
+    
+            if graph[nx][ny] == 0:
+                graph[nx][ny]=graph[x][y]+1
                 queue.append((nx, ny))
 
 bfs()
-
-for x in graph:
-    if 0 in x:
-        print(-1)
-        sys.exit(0)
-
-print(max(map(max, graph))-1)
+print(find_max())
