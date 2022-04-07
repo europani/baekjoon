0b9376550ebadab76d27317b1d27c6ca4040136f
SANGIL PARK <europani1212@gmail.com>
2022-04-07 10:58:14 +0900

    [Silver I] Title: 경로 찾기, Time: 296 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(input().split()) for _ in range(n)]
+
+# 플루워드 워셜
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][k]=='1' and graph[k][j]=='1':
+                graph[i][j]='1'
+
+for x in graph:
+    print(' '.join(x), end='\n')
+  
