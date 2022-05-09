16c30dc492b90c783eaf73c42ac1123ea9ea49db
SANGIL PARK <europani1212@gmail.com>
2022-05-09 15:12:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 360 ms, Memory: 131088 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,21 +1,26 @@
-import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
+from collections import deque
 
 n = int(input())
 graph = [list() for _ in range(n+1)]
 parent = [-1] * (n+1)
 
+def bfs(start):
+    queue = deque([start])
+    parent[start]=start
+
+    while queue:
+        now = queue.popleft()
+
+        for i in graph[now]:
+            if parent[i]==-1:
+                parent[i]=now
+                queue.append(i)
+        
 for _ in range(n-1):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
+bfs(1)
 
-def dfs(x):
-    for i in graph[x]:
-        if parent[i]==-1:
-            parent[i]=x
-            dfs(i)
-dfs(1)
 for i in range(2, n+1):
     print(parent[i])
