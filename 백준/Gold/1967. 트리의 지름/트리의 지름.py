f2f943095057a586184b19cf49054b8baef0c7be
SANGIL PARK <europani1212@gmail.com>
2022-05-12 13:43:46 +0900

    [Gold IV] Title: 트리의 지름, Time: 504 ms, Memory: 43820 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -4,7 +4,7 @@ sys.setrecursionlimit(10**9)
 
 n = int(input())
 graph = [list() for _ in range(n+1)]
-v, c = 0, 0
+v, d = 0, 0
 
 for _ in range(n-1):
     a, b, c = map(int, input().split())
@@ -12,11 +12,11 @@ for _ in range(n-1):
     graph[b].append((a, c))
 
 def dfs(x, sum):
-    global v, c
+    global v, d
     visited[x]=True
 
-    if sum >= c:
-        v, c = x, sum
+    if sum > d:
+        v, d = x, sum
 
     for now, cost in graph[x]:
         if not visited[now]:
@@ -26,4 +26,4 @@ visited = [False] * (n+1)
 dfs(1, 0)
 visited = [False] * (n+1)
 dfs(v, 0)
-print(c)
+print(d)
