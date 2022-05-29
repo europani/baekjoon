ff63d6c1fad47a951ad53406e1112ea96128f59d
SANGIL PARK <europani1212@gmail.com>
2022-05-29 16:26:17 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 740 ms, Memory: 54176 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static int N, M, result;
+    static int[] parent;
+    static ArrayList<Edge> edges = new ArrayList<>();
+
+    static class Edge implements Comparable<Edge> {
+        int from;
+        int to;
+        int cost;
+
+        public Edge(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            if(this.cost < o.cost)
+                return -1;
+            else if(this.cost == o.cost)
+                return 0;
+            else
+                return 1;
+        }
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.valueOf(st.nextToken());
+            int b = Integer.valueOf(st.nextToken());
+            int c = Integer.valueOf(st.nextToken());
+
+            edges.add(new Edge(a, b, c));
+        }
+    }
+
+    static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a < b) {
+            parent[b] = a;
+        } else {
+            parent[a] = b;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        Collections.sort(edges);
+
+        for (Edge e : edges) {
+            if (find(e.from) != find(e.to)) {
+                union(e.from, e.to);
+                result += e.cost;
+            }
+        }
+        System.out.println(result);
+    }
+}
