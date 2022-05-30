52c4cf2b25827cd004c2a301e109202375239916
SANGIL PARK <europani1212@gmail.com>
2022-05-30 10:14:56 +0900

    [level 2] Title: 전화번호 목록, Time: 380.76 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,23 +1,16 @@
+// 맞춘 후 풀이참조
 import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Map<String, Integer> map = new HashMap();
+        Arrays.sort(phone_book);
         
-        for (String num : phone_book) {
-            map.put(num, 0);
-        }
-        
-        for (String num : phone_book) {
-            String tmp = "";
-            for (int i=0; i < num.length()-1; i++) {
-                tmp+=num.charAt(i);
-
-                if (map.containsKey(tmp)) {
-                    return false;
-                }
+        for (int i=0; i < phone_book.length-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
             }
         }
+
         
         
         return true;
